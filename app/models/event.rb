9216a9643a837e45fea25a49023e63c31ae9c16b
Div685 <divyeshpatel685@gmail.com>
2021-01-03 15:16:36 +0300

    Update user and event model
    - add foreign_key, scope for date
    - update routes
    - update index, show.html page for attendance and events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :creator, class_name: 'User', foreign_key: :user_id
   has_many :Attendances
   has_many :attendees, through: :Attendances
+
+  scope: :upcoming, -> { where("date >= ?", Date.today) }
+  scope: :past, -> { where("date < ?", Date.today) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :events
+  resources :attendances
 
   get 'sign_in' => 'sessions#new', as: 'sign_in'
   get 'sign_out' => 'sessions#destroy', as: 'sign_out'
