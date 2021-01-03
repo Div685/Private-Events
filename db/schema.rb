4ff6b6c97d8ccdc0b887fe4ccb9a514bb1af98f9
Div685 <divyeshpatel685@gmail.com>
2021-01-03 19:42:20 +0300

    Add status to attenances using migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_02_103143) do
+ActiveRecord::Schema.define(version: 2021_01_03_164015) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "attendee_id"
     t.integer "attended_event_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "status"
   end
 
   create_table "events", force: :cascade do |t|
