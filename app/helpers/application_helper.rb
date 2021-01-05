c63e2acdbc12072634e4c6c7ad6cf16ca9fac5ff
Div685 <divyeshpatel685@gmail.com>
2021-01-05 12:27:37 +0300

    Add logic to helper class
    - separated logic from view file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+    def nav_links
+        if current_user
+            content_tag(:div, link_to('Create Event', new_event_path, class: "text-decoration-none"), class: "btn btn-light mr-2") + 
+            content_tag(:div , link_to(current_user.name, current_user, class: "text-white font-weight-bold text-decoration-none"), class: "text-white mr-2") +
+            content_tag(:div, link_to('Sign Out', sign_out_path, class: "text-white font-weight-bold text-decoration-none"), class: "ml-2")
+        else
+            content_tag(:div, link_to('Sign In', sign_in_path, class: "text-white"), class: "") +
+            content_tag(:div, link_to('Sign Up', sign_up_path), class: "btn btn-light ml-2")
+        end
+    end
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,15 @@
 module EventsHelper
+
+    def invite_people
+        if current_user == @event.creator
+            content_tag(:div, link_to('Invite People', attendances_path(event_id: @event.id), class: "btn btn-primary text-uppercase"))
+
+        else
+            if @is_attendy
+                content_tag(:div, link_to('Withdraw', attendances_path(event_id: @event.id), method: :patch, class: "btn btn-primary text-uppercase"))
+            else
+                content_tag(:div, link_to('Join Event', attendances_path(event_id: @event.id), method: :patch,  class: "btn btn-primary text-uppercase"))
+            end
+        end
+    end
 end
