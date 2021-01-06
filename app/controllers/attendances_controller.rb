4e0321419bb2cd9d38932763b539ccc29ab454ba
Adewunmi97 <shonibareadewunmicomfort@gmail.com>
2021-01-06 09:51:12 +0100

    update attendances view file

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -3,22 +3,6 @@ class AttendancesController < ApplicationController
     @attendances = Attendance.all
   end
 
-  def create
-    user_id = params[:user_id]
-    event = Event.find(params[:event_id])
-    @invite = Attendance.new(attended_event_id: event.id, attendee_id: user_id)
-
-    if @invite.save
-      @invite.invited!
-      flash[:notice] = 'INVITATION IS SENT SUCCESSFULLY!'
-    else
-      flash[:alert] = 'SOMETHING WENT WRONG!.'
-    end
-
-    redirect_to attendances_path(event_id: event.id)
-  end
-
-  def show
-  end
+  
 
 end
